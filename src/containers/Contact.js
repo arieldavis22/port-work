660a5409eb624a774bf34602eb2bc2f19fcf258c
arieldavis22 <amanichai100@gmail.com>
2020-12-30 22:31:17 -0500

    form work

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -4,8 +4,19 @@ class Contact extends Component {
     constructor(props) {
         super(props);
         this.state = {  
-
+            name: "",
+            email: "",
+            message: ""
         }
+    }
+
+    handleChange = (event) => {
+        const target = event.target
+        const name = target.name
+        this.setState({
+            [name]: event.target.value
+        })
+        console.log(this.state);
     }
     render() { 
         return (  
@@ -13,17 +24,17 @@ class Contact extends Component {
                 <form>
                     <label>
                         First & Last Name:
-                        <input type="text" name="name"/>
+                        <input type="text" name="name" onChange={this.handleChange}/>
                     </label>
                     <br/>
                     <label>
                         Email Address:
-                        <input type="email" name="address"/>
+                        <input type="email" name="address" onChange={this.handleChange}/>
                     </label>
                     <br/>
                     <label>
                         Message:
-                        <textarea/>
+                        <textarea onChange={this.handleChange}/>
                     </label>
                 </form>
             </div>
