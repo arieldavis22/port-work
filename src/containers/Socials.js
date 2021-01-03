ce17ab26c3ac8a8f715a29db7073d5c68d60fb42
arieldavis22 <amanichai100@gmail.com>
2021-01-02 22:52:05 -0500

    font stuff

--- src/containers/Socials.js
+++ src/containers/Socials.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faGithub } from '@fortawesome/f?ee-solid-svg-icons'
 class Socials extends Component {
     constructor(props) {
         super(props);
@@ -8,6 +10,7 @@ class Socials extends Component {
         return (  
             <div>
                 Socials
+                <FontAwesomeIcon icon="coffee" />
             </div>
         );
     }
