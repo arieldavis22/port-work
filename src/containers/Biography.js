1f175606c6764380ab99a8bf0a5f7fe9709730ca
Ariel Davis <adavis@interplaylearning.com>
2021-01-22 16:56:58 -0500

    fade in

--- src/containers/Biography.js
+++ src/containers/Biography.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import pic from '../pictures/pfp.png'
 import '../css/Biography.css'
+import Fade from 'react-reveal/Fade';
 class Biography extends Component {
     constructor(props) {
         super(props);
@@ -8,14 +9,20 @@ class Biography extends Component {
     }
     render() { 
         return (  
-            <div className={'bio-container'}>
-                <img src={pic}  alt="error" className={"profile-img"}/>
-                <div>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <>
+            <Fade top duration={3000}>
+                <div className={'bio-container'}>
+                    <img src={pic}  alt="error" className={"profile-img"}/>
+                    <Fade delay={1000}>
+                        <div className={'bio-desc'}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </div>
+                    </Fade>
                 </div>
-            </div>
+            </Fade>
+            </>
         );
     }
 }
- 
+
 export default Biography;
